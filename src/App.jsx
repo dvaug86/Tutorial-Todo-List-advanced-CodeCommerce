beb14ec7c188f4339d9561b2d6488d5be2073686
David Vaughan <dvaug86@gmail.com>
2023-12-12 18:28:20 -0500

    added read function

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 import Todo from "./components/Todo";
+import { db } from "./firebase";
+import {
+  QuerySnapshot,
+  collection,
+  onSnapshot,
+  query,
+} from "firebase/firestore";
 
 const style = {
   bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
-    container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
-    heading: `text-3xl font-bold text-center text-gray-800 p-2`,
-    form: `flex justify-between`,
-    input: `border p-2 w-full text-xl`,
-    button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
-    count: `text-center p-2`,
+  container: `bg-slate-100 max-w-[500px] w-full m-auto rounded-md shadow-xl p-4`,
+  heading: `text-3xl font-bold text-center text-gray-800 p-2`,
+  form: `flex justify-between`,
+  input: `border p-2 w-full text-xl`,
+  button: `border p-4 ml-2 bg-purple-500 text-slate-100`,
+  count: `text-center p-2`,
 };
 
 function App() {
-  const [todos, setTodos] = useState(["Learn React", "Grind Leetcode"]);
+  const [todos, setTodos] = useState([]);
+
+  // create todo
+
+  // read todo
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsubscribe = onSnapshot(q, (QuerySnapshot) => {
+      let todosArr = [];
+      QuerySnapshot.forEach((doc) => {
+        todosArr.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArr);
+    });
+    return () => unsubscribe();
+  }, []);
+  // update todo
+  // delete todo
 
   return (
     <div className={style.bg}>
