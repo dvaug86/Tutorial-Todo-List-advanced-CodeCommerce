edb3e82469950c220bc73a992120a80436fe17f8
David Vaughan <dvaug86@gmail.com>
2023-12-12 06:41:02 -0500

    added styles with tailwind

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,6 +1,13 @@
 import React from "react";
-import {FaRegTrashAlt} from 'react-icons/fa'
-const style = {};
+import { FaRegTrashAlt } from "react-icons/fa";
+const style = {
+  li: `flex justify-between bg-slate-200 p-4 my-2 capitalize`,
+  liComplete: `flex justify-between bg-slate-400 p-4 my-2 capitalize`,
+  row: `flex`,
+  text: `ml-2 cursor-pointer`,
+  textComplete: `ml-2 cursor-pointer line-through`,
+  button: `cursor-pointer flex items-center`,
+};
 
 const Todo = ({ todo }) => {
   return (
@@ -9,7 +16,7 @@ const Todo = ({ todo }) => {
         <input type="checkbox" />
         <p className={style.text}>{todo}</p>
       </div>
-      <button>{<FaRegTrashAlt/>}</button>
+      <button>{<FaRegTrashAlt />}</button>
     </li>
   );
 };
